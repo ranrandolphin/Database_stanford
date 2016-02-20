d5f07bc3e096390fefc4cec3067cf2f13af04411
RAN WEI <ranranw1023@gmail.com>
2016-02-20 16:27:49 -0500

    Create SQL_movie_rating_query.sql

---
+++ SQL_movie_rating_query.sql
@@ -0,0 +1,8 @@
+-- Movie ( mID, title, year, director ) 
+-- English: There is a movie with ID number mID, a title, a release year, and a director. 
+
+-- Reviewer ( rID, name ) 
+-- English: The reviewer with ID number rID has a certain name. 
+
+-- Rating ( rID, mID, stars, ratingDate ) 
+-- English: The reviewer rID gave the movie mID a number of stars rating (1-5) on a certain ratingDate. 
