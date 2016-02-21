3007112eee4c0ea1f2c6031e6ce9825c9d77230d
RAN WEI <ranranw1023@gmail.com>
2016-02-21 04:10:48 -0500

    Update SQL_Social_Network_query.sql

--- SQL_Social_Network_query.sql
+++ SQL_Social_Network_query.sql
@@ -26,6 +26,11 @@ WHERE h1.grade - h2.grade >= 2;
 
 -- Q3. For every pair of students who both like each other, return the name and grade of both students. 
 -- Include each pair only once, with the two names in alphabetical order. 
+SELECT h1.name, h1.grade, h2.name, h2.grade
+FROM Likes l1 JOIN Likes l2 ON l1.ID1 = l2.ID2 AND l1.ID2 = l2.ID1
+JOIN Highschooler h1 ON h1.ID = l1.ID1
+JOIN Highschooler h2 ON h2.ID = l1.ID2
+WHERE h1.name < h2.name;
 
 -- Q4.Find all students who do not appear in the Likes table (as a student who likes or is liked) and return their names and grades. 
 -- Sort by grade, then by name within each grade. 
